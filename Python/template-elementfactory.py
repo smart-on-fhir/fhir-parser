ba6aa8c8d7dbb72a804a146cbf31ee81f861ab9a
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2016-03-31 02:12:02 +0600

    [Python] improve elementfactory class

--- Python/template-elementfactory.py
+++ Python/template-elementfactory.py
@@ -10,19 +10,32 @@ from . import element
 class FHIRElementFactory(object):
     """ Factory class to instantiate resources by resource name.
     """
-    
+
     @classmethod
     def instantiate(cls, resource_name, jsondict):
         """ Instantiate a resource of the type correlating to "resource_name".
-        
+
         :param str resource_name: The name/type of the resource to instantiate
         :param dict jsondict: The JSON dictionary to use for data
-        :returns: A resource of the respective type or `Element`
+        :returns: A resource of the respective type or None
         """
+
+        klass = cls.get_class(resource_name)
+        if klass:
+            return klass(jsondict)
+        return None
+
+    @classmethod
+    def get_class(cls, resource_name):
+        """ Get resource class of the type correlating to "resource_name".
+
+        :param str resource_name: The name/type of the resource to instantiate
+        :returns: A resource class of the respective type or None
+        """
+
         {%- for klass in classes %}{% if klass.resource_name %}
         if "{{ klass.resource_name }}" == resource_name:
             from . import {{ klass.module }}
-            return {{ klass.module }}.{{ klass.name }}(jsondict)
+            return {{ klass.module }}.{{ klass.name }}
         {%- endif %}{% endfor %}
-        return element.Element(json)
-
+        return None
