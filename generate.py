1982952ffec203ef9fc7d7eb4b7b939f4bddd4ab
Valery Vishnevskiy <v.v.vishnevskiy@gmail.com>
2016-03-31 13:42:38 +0600

    Add additional parameters

--- generate.py
+++ generate.py
@@ -14,6 +14,9 @@ import argparse
 def args():
     parser = argparse.ArgumentParser(description='FHIR models generator')
     parser.add_argument('-f', '--force', action='store_true', help='To force a redownload of the spec')
+    parser.add_argument('-r', '--resources', action='store_true', help='Generate resource models')
+    parser.add_argument('-e', '--element_factory', action='store_true', help='Generate elementfactory')
+    parser.add_argument('-t', '--tests', action='store_true', help='Generate tests')
     parser.add_argument('--ln', required=True, help='Choose the language', choices=['python', 'swift'])
     parser.add_argument('--cache', help='The path to the directory with all downloaded files', default='downloads')
     parser.add_argument('--output', help='The path to the directory with all generated models', default='models')
@@ -25,8 +28,14 @@ if '__main__' == __name__:
 
     if params['ln'] == 'python':
         from Python import settings
+        settings.write_resources = params['resources']
+        settings.write_factory = params['element_factory']
+        settings.write_unittests = params['tests']
     elif params['ln'] == 'swift':
         from Swift import settings
+        settings.write_resources = params['resources']
+        settings.write_factory = params['resources']
+        settings.write_unittests = params['tests']
     else:
         sys.exit(1)
 
