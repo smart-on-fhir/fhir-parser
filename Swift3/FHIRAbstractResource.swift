70ba7571b78fc753c669e07dbaf13580d27dcaae
Pascal Pfiffner <phase.of.matter@gmail.com>
2016-12-13 19:19:14 +0100

    [Swift3] Only inspect resourceType in FHIRAbstractResource

--- Swift3/FHIRAbstractResource.swift
+++ Swift3/FHIRAbstractResource.swift
@@ -24,6 +24,28 @@ open class FHIRAbstractResource: FHIRAbstractBase {
 	}
 	var __server: FHIRServer?
 	
+	
+	// MARK: - FHIRJSONType
+	
+	/**
+	Tries to find `resourceType` by inspecting the JSON dictionary, then instantiates the appropriate class for the specified resource type;
+	instantiates the receiver's class otherwise.
+	
+	- note: If the factory does not return a subclass of the receiver, will discard the factory-created instance and use
+	`self.init(json:owner:)` instead.
+	
+	- parameter json:  A FHIRJSON decoded from a JSON response
+	- parameter owner: The FHIRAbstractBase owning the new instance, if appropriate
+	- returns:         If possible the appropriate FHIRAbstractBase subclass, instantiated from the given JSON dictionary, Self otherwise
+	- throws:          FHIRValidationError
+	*/
+	public final override class func instantiate(from json: FHIRJSON, owner: FHIRAbstractBase?) throws -> Self {
+		if let type = json["resourceType"] as? String {
+			return try factory(type, json: json, owner: owner, type: self)
+		}
+		return try self.init(json: json, owner: owner)		// must use 'required' init with dynamic type
+	}
+	
 	/**
 	The Resource, in contrast to the base element, definitely wants "resourceType" to be present. Will return an error complaining about it
 	missing if it's not present.
