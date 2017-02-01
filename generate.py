fd8e453409458a83863544841c75beeb27554840
Pascal Pfiffner <phase.of.matter@gmail.com>
2017-02-01 14:18:49 +0100

    Add -l switch to generate.py
    It’s soon time to use argparse…

--- generate.py
+++ generate.py
@@ -16,12 +16,14 @@ _cache = 'downloads'
 if '__main__' == __name__:
     force = len(sys.argv) > 1 and '-f' in sys.argv
     dry = len(sys.argv) > 1 and ('-d' in sys.argv or '--dry-run' in sys.argv)
+    load_only = len(sys.argv) > 1 and ('-l' in sys.argv or '--load-only' in sys.argv)
     
     # assure we have all files
     loader = fhirloader.FHIRLoader(settings, _cache)
     spec_source = loader.load(force)
     
     # parse
-    spec = fhirspec.FHIRSpec(spec_source, settings)
-    if not dry:
-	    spec.write()
+    if not load_only:
+        spec = fhirspec.FHIRSpec(spec_source, settings)
+        if not dry:
+            spec.write()
