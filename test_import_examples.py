5351d788caa7dead54fdc74f92c4afb3551e3ac3
Julian David Rath <julian@skalarsystems.com>
2020-05-13 19:58:34 +0200

    simple test to see if all examples are working.

---
+++ test_import_examples.py
@@ -0,0 +1,16 @@
+import pathlib
+import json
+
+from pydantic.error_wrappers import ValidationError
+
+import r4
+
+for file_path in pathlib.Path("./downloads").glob("*-example.json"):
+    print(file_path)
+    with file_path.open() as f_in:
+        doc = json.load(f_in)
+        try:
+            doc = r4.from_dict(doc)
+            # print(doc.id)
+        except ValidationError as er:
+            print("...did not validate", er)
