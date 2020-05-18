8041cbd3d76c5bfc07c55c522e4ace9cf18a65f9
Julian David Rath <julian@skalarsystems.com>
2020-05-18 15:23:54 +0200

    added `from_dict`
    Co-authored-by: Jakob Auer <jakob.auer@gmail.com>

--- fhirzeug/generators/python_pydantic/templates/resource_footer.py
+++ fhirzeug/generators/python_pydantic/templates/resource_footer.py
@@ -14,5 +14,13 @@ for subclass in inheritors(FHIRAbstractBase):
     subclass.update_forward_refs()
 
 
-for subclass in inheritors(FHIRAbstractResource):
-    subclass.update_forward_refs()
+RESOURCE_TYPE_MAP: typing.Dict[str, Resource] = {}
+for subclass in inheritors(Resource):
+    RESOURCE_TYPE_MAP[subclass.__name__] = subclass
+
+
+def from_dict(dict_: dict):
+    """Factory to load resources directly.
+    
+    The resources will be instaciated based on their resourceType property."""
+    return RESOURCE_TYPE_MAP[dict_["resourceType"]](**dict_)
